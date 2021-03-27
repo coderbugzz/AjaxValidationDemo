a0bf82c93a1c33c5518d7bc3ddb67bf9e52d976a
Regie S. Baquero <regiebaquero@gmail.com>
2021-03-27 20:17:49 +0800

    Add project files.

---
+++ AjaxValidationDemo/Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AjaxValidationDemo.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
